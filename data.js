7758f51868f3a5a6d1786a42734636b2d72ffb67
Espezzialy <espezzialy@outlook.com>
2021-01-21 15:01:03 -0300

    :rocket: first commit

---
+++ data.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        id: "vKazFN3nbhs",
+        title: "Universo Kotlin | RocketSeat",
+        duration: "37 min",
+        price: "Free"
+    },
+    
+    {
+        id: "TtZfCLViuLo",
+        title: "Video 2 ",
+        duration: "37 min",
+        price: "Free"
+    },
+
+    {
+        id: "7vSwoy63cy0",
+        title: "Video 3",
+        duration: "37 min",
+        price: "Free"
+    },
+
+    {
+        id: "jLlOnsIapdQ",
+        title: "Video 4",
+        duration: "37 min",
+        price: "Free"
+    },
+
+    {
+        id: "Fjmd-uWir0Q",
+        title: "Video 5",
+        duration: "37 min",
+        price: "Free"
+    },
+
+    {
+        id: "MVUmD2U5JsM",
+        title: "Video 6",
+        duration: "37 min",
+        price: "Free"
+    },
+
+]
